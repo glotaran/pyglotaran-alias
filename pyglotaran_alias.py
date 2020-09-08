14b90e5573026aaf0bfcc9796e99fd831eab183d
s-weigand <s.weigand.phy@gmail.com>
2020-09-08 12:53:02 +0200

    Refactored code to import glotaran with the __import__ fuction
    instead of the import statement.
    This way glotaran won't be added to globals.

--- pyglotaran_alias.py
+++ pyglotaran_alias.py
@@ -1,16 +1,17 @@
 """Enables 'import pyglotaran' as well as the default 'import glotaran'."""
 
 import sys
+from importlib.util import find_spec
 
-try:
-    import glotaran  # noqa:  F401
+if find_spec("glotaran"):
+    __import__("glotaran")
 
     modules_to_update = {}
     for fullname, glotaran_module in sys.modules.items():
         if fullname == "glotaran" or fullname.startswith("glotaran."):
             modules_to_update[f"py{fullname}"] = glotaran_module
     sys.modules.update(modules_to_update)
-except ImportError:
+else:
     raise ImportError(
         """'pyglotaran_alias' only provides an alias to 'import glotaran' with 'import pyglotaran'.
         In order for this to work you need to install pyglotaran in you current environment.
--- test_pyglotaran_alias.py
+++ test_pyglotaran_alias.py
@@ -1,15 +1,36 @@
+import importlib.util
 import sys
 
 import pytest
 
 
 def test_no_initial_pyglotaran():
+    """Without 'import pyglotaran_alias' pyglotaran can't be imported."""
     with pytest.raises(ImportError):
         import pyglotaran  # noqa:  F401
 
 
+def test_exception_if_glotaran_is_missing(monkeypatch):
+    """Raise Exception if glotaran isn't installed."""
+
+    def mock_find_spec(*args, **kwargs):
+        pass
+
+    monkeypatch.setattr(importlib.util, "find_spec", mock_find_spec)
+    with pytest.raises(ImportError, match=r"you need to install pyglotaran"):
+        import pyglotaran_alias  # noqa:  F401
+
+
+def test_glotaran_import_not_leeking_out():
+    """Glotaran isn't imported normally and thus not in globals."""
+    import pyglotaran_alias  # noqa:  F401
+
+    assert "glotaran" not in globals().keys()
+
+
 def test_import_works():
-    import pyglotaran_alias  # noqa:  F401 isort:skip
+    """Check that 'import pyglotaran' works and 'pyglotaran' is an actual alias to 'glotaran'."""
+    import pyglotaran_alias  # noqa: F401 isort:skip
     import glotaran  # noqa:  F401
     import pyglotaran  # noqa:  F401
 
@@ -19,8 +40,7 @@ def test_import_works():
 
     glotaran_modules = tuple(
         filter(
-            lambda name: name == "glotaran" or name.startswith("glotaran."),
-            loaded_module_names,
+            lambda name: name == "glotaran" or name.startswith("glotaran."), loaded_module_names,
         )
     )
 
