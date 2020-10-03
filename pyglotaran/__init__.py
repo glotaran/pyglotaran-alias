3da4cbfc3ba759b145fb13e11e12d32ff488506e
Sebastian Weigand <s.weigand.phy@gmail.com>
2020-10-04 01:44:30 +0200

    Drop need to call 'import pyglotaran_alias' before 'import pyglotaran' (#4)
    * Changed packaging so 'import pyglotaran' works out of the box
    
    * Added description how it works and known errors
    
    * Refined descripton of linter problem
    
    * Added editor config and .gitattributes
    
    * Reset version to 0.0.1, added __version__ to __init__.py and added
    
    bump2version config
    
    * Set the indentation style of setup.cfg  to tab
    
    since this is the way it gets transformed by bump2version, which parses back configparsers sting
    
    * Added pre-commit autoupdate workflow
    
    * Removed PyPy as supported implementation
    
    * Fixed typo
    
    * Added project urls
    
    * Added python dependencies to dependabot and fixed version of glotaran
    
    * Reactivated deployment in CI-CD workflow
    
    I will keep in mind to create a new token
    
    * Removed __version__ attr of the module, since it gets overwritten anyway
    
    * Updated repo link to isort
    
    * Removed bump2version and it's config since it doesn't work with setup.cfg
    
    See https://github.com/c4urself/bump2version/pull/83
    
    * Updated isort repo rev and added .md to codespell checked files

--- pyglotaran/__init__.py
+++ pyglotaran/__init__.py
@@ -1,9 +1,8 @@
 """Enables 'import pyglotaran' as well as the default 'import glotaran'."""
-
 import sys
 from importlib.util import find_spec
 
-if find_spec("glotaran"):
+if find_spec("glotaran") and "pyglotaran" not in globals().keys():
     __import__("glotaran")
 
     modules_to_update = {}
@@ -11,6 +10,13 @@ if find_spec("glotaran"):
         if fullname == "glotaran" or fullname.startswith("glotaran."):
             modules_to_update[f"py{fullname}"] = glotaran_module
     sys.modules.update(modules_to_update)
+    pyglotaran = sys.modules["glotaran"]
+    # ensure that variables used here aren't visible in autocomplete
+    del locals()["sys"]
+    del locals()["find_spec"]
+    del locals()["fullname"]
+    del locals()["glotaran_module"]
+    del locals()["modules_to_update"]
 else:
     raise ImportError(
         """'pyglotaran_alias' only provides an alias to 'import glotaran' with 'import pyglotaran'.
