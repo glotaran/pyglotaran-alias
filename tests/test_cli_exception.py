f455dc8d15721625cbc0b1bc895e3498dc7c880d
Sebastian Weigand <s.weigand.phy@gmail.com>
2020-10-06 17:42:54 +0200

    Use the pyglotaran package as cli entrypoint (#9)
    * Use the pyglotaran package as cli entrypoint
    
    This way the __init__.py of pyglotaran-alias will be called and if pyglotaran isn't installed it will raise the proper ImportError, telling users to install pyglotaran.
    
    * Added tests for cli raising the proper exception
    
    * Added test for working cli

---
+++ tests/test_cli_exception.py
@@ -0,0 +1,11 @@
+import re
+import subprocess
+
+
+def test_cli_raises_proper_exeption():
+    """Test that cli Raises proper exception when pyglotaran isn't installed."""
+    output = subprocess.run(
+        "pyglotaran", shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE
+    )
+    assert re.search(b"'pip install pyglotaran'", output.stderr) is not None
+    assert output.stdout == b""
